dd33ba1e179879beb1d7025586e7f8b818eabbf6
ofix <shb8845369@gmail.com>
2019-10-22 23:55:08 +0800

    fix wxXmlResource::LoadObject Error!

--- eveApp.cpp
+++ eveApp.cpp
@@ -16,12 +16,16 @@
 #endif //__BORLANDC__
 
 #include "eveApp.h"
+#include <wx/xrc/xmlres.h>
 #include "ConfigDialog.h"
 
 IMPLEMENT_APP(eveApp);
 
 bool eveApp::OnInit()
 {
+    //Please reference following link(https://docs.wxwidgets.org/2.8.12/wx_xrcoverview.html#xrcoverview)
+    wxXmlResource::Get()->InitAllHandlers();
+    wxXmlResource::Get()->Load("ConfigDialog.xrc");
     ConfigDialog* dlg = new ConfigDialog(0L);
     dlg->SetIcon(wxICON(aaaa)); // To Set App Icon
     dlg->Show();
